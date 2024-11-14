26d0d904a873ff6efc6e85838d8f06f55750acb8
Bruno Aquino <theodorodeaquino@gmail.com>
2024-11-13 21:48:41 -0300

    Entendendo os tipo kotlin

--- first/src/main/kotlin/Main.kt
+++ first/src/main/kotlin/Main.kt
@@ -3,6 +3,24 @@ fun main() {
 
     var name = "Ubuntu"
     var version = "Estou usando a versão 24.04 LTS"
+    val byte: Byte = 8
     println(name)
     println(version)
+    println(byte)
+    // VAL é uma variavel imutavel
+    // Declarando o tipo de variavel
+
+    var preco: Double = 1.99
+    val idade: Int = 38
+    val marca: String = "Android"
+
+    //:: class para mostrar o tipo de dado
+
+    println(marca::class)
+    println(idade::class)
+    print(preco::class)
+    /* forçando erro de compilação
+
+    var result = 10 / 0
+    println(result)*/
 }
