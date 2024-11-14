36617173b06cd286b3bdd28d48a837af8b569964
Bruno Aquino <theodorodeaquino@gmail.com>
2024-11-14 05:25:38 -0300

    Primeiro exercicio com variaveis

---
+++ first/src/main/kotlin/Desafio01.kt
@@ -0,0 +1,22 @@
+import javax.swing.text.StyledEditorKit.BoldAction
+
+class Desafio01 {
+
+    fun messager(){
+
+        val name : String = "Bruno Aquino"
+        var age : Int = 38
+        val firstLetter : Char = 'B'
+        var height : Double = 1.80
+        var weight : Double = 95.00
+        val dev : Boolean = true
+
+
+        println(name)
+        println(age)
+        println(firstLetter)
+        println(height)
+        println(weight)
+        println(dev)
+    }
+}
--- first/src/main/kotlin/Main.kt
+++ first/src/main/kotlin/Main.kt
@@ -1,12 +1,17 @@
+import javax.swing.text.StyledEditorKit.BoldAction
+
 fun main() {
     println("Hello World!")
 
     var name = "Ubuntu"
     var version = "Estou usando a versão 24.04 LTS"
     val byte: Byte = 8
+
+    val long: Long = 12345678999
     println(name)
     println(version)
     println(byte)
+    println(long)
     // VAL é uma variavel imutavel
     // Declarando o tipo de variavel
 
@@ -14,13 +19,22 @@ fun main() {
     val idade: Int = 38
     val marca: String = "Android"
 
+
+    val char: Char = '@'
+    val boolean : Boolean = true
+
     //:: class para mostrar o tipo de dado
 
     println(marca::class)
     println(idade::class)
     print(preco::class)
+    println(char::class)
+    println(boolean::class)
     /* forçando erro de compilação
 
     var result = 10 / 0
     println(result)*/
+
+    Desafio01().messager()
+
 }
